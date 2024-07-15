64e15b81b1ed05a59468e9951ef5cfa3117dae27
Yi Doyoon <yidoyoon@yidoyoon.com>
2024-07-15 19:50:22 +0900

    feat: add local key delete function

--- content.js
+++ content.js
@@ -1,25 +1,12 @@
-// Popover API https://chromestatus.com/feature/5463833265045504
-
-(async () => {
-  const nav = document.querySelector('.upper-tabs > nav');
-
-  const { tip } = await chrome.runtime.sendMessage({ greeting: 'tip' });
-
-  const tipWidget = createDomElement(`
-    <button type="button" popovertarget="tip-popover" popovertargetaction="show" style="padding: 0 12px; height: 36px;">
-      <span style="display: block; font: var(--devsite-link-font,500 14px/20px var(--devsite-primary-font-family));">Tip</span>
-    </button>
-  `);
-
-  const popover = createDomElement(
-    `<div id='tip-popover' popover style="margin: auto;">${tip}</div>`
-  );
-
-  document.body.append(popover);
-  nav.append(tipWidget);
-})();
-
-function createDomElement(html) {
-  const dom = new DOMParser().parseFromString(html, 'text/html');
-  return dom.body.firstElementChild;
+// content.js
+function checkForSolved() {
+  const elements = document.querySelectorAll('.text-body.flex.flex-none.items-center.gap-1');
+  for (let element of elements) {
+    if (element.textContent.includes('Solved')) {
+      chrome.runtime.sendMessage({ action: 'deleteLocalStorage' });
+      break;
+    }
+  }
 }
+
+checkForSolved();
